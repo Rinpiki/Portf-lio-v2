5e460cd7eca8c18114ee1c8f12126d0e7ad73bc9
Marcos Pablo <rinpikibr@gmail.com>
2023-02-18 15:40:23 -0300

    test: bug build test

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+declare module 'react-reveal/Fade' {
+  const Fade: unknown
+  export default Fade
+}
+
+declare module 'react-reveal/Jump' {
+  const Jump: unknown
+  export default Jump
+}
